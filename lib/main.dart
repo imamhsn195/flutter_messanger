bccbb072ae02fb00a578b674fa11b22fef61fd65
imamhsn195 <imamhsn195@gmail.com>
2021-12-27 17:05:08 +0400

    Add a UI for composing messages

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,52 @@ class FriendlyChatApp extends StatelessWidget {
     );
   }
 }
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+  Widget _buildTextComposer(){
+    return IconTheme(
+        data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+        child:  Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: const InputDecoration.collapsed(hintText: 'Send Message'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textController.text),
+            ),
+          )
+        ],
+      ),
+    )
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Messenger Chats"),
       ),
+      body: _buildTextComposer(),
     );
+  }
+
+  void _handleSubmitted(String value) {
+    _textController.clear();
   }
 }
