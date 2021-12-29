4dd31ed7abbfe6f5b2b685f9631cb56b4211ce05
imamhsn195 <imamhsn195@gmail.com>
2021-12-30 00:04:39 +0400

    Holf done

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    const FriendlyChatApp()
-  );
+void main() {
+  runApp(const FriendlyChatApp());
 }
 
 class FriendlyChatApp extends StatelessWidget {
   const FriendlyChatApp({
     Key? key,
   }) : super(key: key);
-
+  final _name = "Imam Hasan";
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Messenger App",
-      home: ChatScreen()
+    return const MaterialApp(title: "Messenger App", home: ChatScreen());
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({required this.text, Key? key}) : super(key: key);
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name[0], style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
+
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
@@ -28,32 +54,33 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
-  Widget _buildTextComposer(){
+  Widget _buildTextComposer() {
     return IconTheme(
         data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
-        child:  Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
-        children: [
-          Flexible(
-            child: TextField(
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
-              decoration: const InputDecoration.collapsed(hintText: 'Send Message'),
-            ),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  decoration:
+                      const InputDecoration.collapsed(hintText: 'Send Message'),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text),
+                ),
+              )
+            ],
           ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 4.0),
-            child: IconButton(
-              icon: const Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textController.text),
-            ),
-          )
-        ],
-      ),
-    )
-    );
+        ));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
