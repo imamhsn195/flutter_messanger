cdf3def2b86de5de55c0fe825ac43b696281906c
imamhsn195 <imamhsn195@gmail.com>
2022-01-05 22:45:37 +0400

    Animate your app

--- lib/main.dart
+++ lib/main.dart
@@ -15,31 +15,38 @@ class FriendlyChatApp extends StatelessWidget {
 }
 
 class ChatMessage extends StatelessWidget {
-  const ChatMessage({required this.text, Key? key}) : super(key: key);
+  const ChatMessage(
+      {required this.text, required this.animationController, Key? key})
+      : super(key: key);
   final String _name = "Imam Hasan";
   final String text;
+  final AnimationController animationController;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: CircleAvatar(child: Text(_name[0])),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(_name, style: Theme.of(context).textTheme.headline6),
-              Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                child: Text(text),
-              )
-            ],
-          )
-        ],
+    return SizeTransition(
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(_name, style: Theme.of(context).textTheme.headline6),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text(text),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -52,12 +59,10 @@ class ChatScreen extends StatefulWidget {
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
-
-  // ignore: prefer_typing_uninitialized_variables
-  var _focusNode;
+  final FocusNode _focusNode = FocusNode();
   Widget _buildTextComposer() {
     return IconTheme(
         data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
@@ -71,7 +76,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   onSubmitted: _handleSubmitted,
                   decoration:
                       const InputDecoration.collapsed(hintText: 'Send Message'),
-                      focusNode: _focusNode,
+                  focusNode: _focusNode,
                 ),
               ),
               Container(
@@ -92,31 +97,44 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: const Text("Messenger Chats"),
       ),
-      body: Column(
-        children: [
-            Flexible(child: ListView.builder(
-              padding: const EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, index) => _messages[index],
-              itemCount: _messages.length,
-            ),
+      body: Column(children: [
+        Flexible(
+          child: ListView.builder(
+            padding: const EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, index) => _messages[index],
+            itemCount: _messages.length,
           ),
-          const Divider(height: 1.0),
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor,),
-              child: _buildTextComposer(),
-            ),
-          ]),
-        );
-      }
+        ),
+        const Divider(height: 1.0),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _buildTextComposer(),
+        ),
+      ]),
+    );
+  }
 
   void _handleSubmitted(String text) {
     _textController.clear();
-    var message = ChatMessage(text: text);
+    var message = ChatMessage(
+      text: text,
+      animationController: AnimationController(
+          duration: const Duration(milliseconds: 700), vsync: this),
+    );
     setState(() {
-      _messages.insert(0, message) ;
+      _messages.insert(0, message);
     });
     _focusNode.requestFocus();
+    message.animationController.forward();
+  }
+
+  void dispse() {
+    for (var message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
   }
 }
